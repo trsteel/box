7a729f780f420e1e94f511808746fcd45f59df1f
trsteel <trsteel@github.com>
2023-07-04 16:58:33 +0800

    fix: remove unsupport method in go 1.18

--- structs/syncmap/sync_map.go
+++ structs/syncmap/sync_map.go
@@ -41,25 +41,6 @@ func (s *SyncMap[K, V]) Delete(key K) {
 	s.Map.Delete(key)
 }
 
-// Swap is a wrap of sync.Map's Swap func with certain K/V type.
-func (s *SyncMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
-	if _previous, _loaded := s.Map.Swap(key, value); _loaded {
-		return _previous.(V), _loaded
-	}
-	var zero V
-	return zero, false
-}
-
-// CompareAndSwap is a wrap of sync.Map's CompareAndSwap func with certain K/V type.
-func (s *SyncMap[K, V]) CompareAndSwap(key K, old, new V) (swapped bool) {
-	return s.Map.CompareAndSwap(key, old, new)
-}
-
-// CompareAndDelete is a wrap of sync.Map's CompareAndDelete func with certain K/V type.
-func (s *SyncMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
-	return s.Map.CompareAndDelete(key, old)
-}
-
 // Range is a wrap of sync.Map's Range func with certain K/V type.
 func (s *SyncMap[K, V]) Range(f func(key K, value V) bool) {
 	s.Map.Range(func(key, value any) bool {
--- structs/syncmap/sync_map_test.go
+++ structs/syncmap/sync_map_test.go
@@ -7,24 +7,6 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func TestSyncMap_CompareAndDelete(t *testing.T) {
-	syncMap := SyncMap[string, string]{}
-	assert.False(t, syncMap.CompareAndDelete("a", "aa")) // before add key
-	syncMap.Store("a", "aa")
-	assert.False(t, syncMap.CompareAndDelete("a", "bb")) // old value mismatch
-	assert.True(t, syncMap.CompareAndDelete("a", "aa"))  // old value match
-	assert.False(t, syncMap.CompareAndDelete("a", "aa")) // after delete
-}
-
-func TestSyncMap_CompareAndSwap(t *testing.T) {
-	syncMap := SyncMap[string, string]{}
-	assert.False(t, syncMap.CompareAndSwap("a", "aa", "bb")) // before add key
-	syncMap.Store("a", "aa")
-	assert.False(t, syncMap.CompareAndSwap("a", "bb", "cc")) // old value mismatch
-	assert.True(t, syncMap.CompareAndSwap("a", "aa", "bb"))  // old value match
-	assert.True(t, syncMap.CompareAndSwap("a", "bb", "cc"))  // old value match
-}
-
 func TestSyncMap_Delete(t *testing.T) {
 	syncMap := SyncMap[string, string]{}
 	// before add
@@ -113,16 +95,4 @@ func TestSyncMap_Store(t *testing.T) {
 	value, ok = syncMap.Load("a")
 	assert.Equal(t, "aa", value)
 	assert.True(t, ok)
-}
-
-func TestSyncMap_Swap(t *testing.T) {
-	syncMap := SyncMap[string, string]{}
-	// first
-	previous, loaded := syncMap.Swap("a", "aa")
-	assert.False(t, loaded)
-	assert.Equal(t, "", previous)
-	// second
-	previous, loaded = syncMap.Swap("a", "bb")
-	assert.True(t, loaded)
-	assert.Equal(t, "aa", previous)
 }
